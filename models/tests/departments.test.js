7d997cccf964143ca7703731ad209cb7cb0e4289
Maciej Kryczka <maciejkryczka@hotmail.com>
2020-01-22 16:28:38 +0100

    add tests to department and employee models

---
+++ models/tests/departments.test.js
@@ -0,0 +1,50 @@
+const Department = require('../department.model.js');
+const expect = require('chai').expect;
+const mongoose = require('mongoose');
+
+describe('Department', () => {
+
+  it('should throw an error if no "name" arg', () => {
+    const dep = new Department({}); 
+
+    dep.validate(err => {
+      expect(err.errors.name).to.exist;
+    });
+  });
+
+  it('should throw an error if "name" is not a string', () => {
+
+    const cases = [{}, []];
+    for(let name of cases) {
+      const dep = new Department({ name });
+  
+      dep.validate(err => {
+        expect(err.errors.name).to.exist;
+      });
+    };
+  });
+
+  it('should throw an error if "name" is shorter than 5 or longer than 20 characters', () => {
+    const cases = ['lor', 'lore', 'Lorem Ipsum dolor mest'];
+    for(let name of cases) {
+      const dep = new Department({name});
+      dep.validate(err => {
+        expect(err.errors.name).to.exist;
+      });
+    }  
+  });
+
+  it('should throw NO error, if "name" i correct', () => {
+    const cases = ['lorem ipsum', 'dolor', 'zdanie co ma 20 lite'];
+    for(let name of cases) {
+      const dep = new Department({name});
+      dep.validate(err => {
+        expect(err).to.not.exist;
+      })
+    }
+  });
+
+  after(() => {
+    mongoose.models = {};
+  });
+})
---
+++ models/tests/employees.test.js
@@ -0,0 +1,34 @@
+const Employee = require('../employee.model');
+const expect = require('chai').expect;
+const mongoose = require('mongoose');
+
+describe('Employee', () => {
+
+  it('should should throw an error, if at least one parameter is missing', () => {
+    const emp = new Employee({})
+    emp.validate(err => {
+      expect(err.errors.firstName && err.errors.lastName && err.errors.department).to.exist;
+    });    
+  })
+
+  it('should throw an error, if any parameter is not a string', () => {
+    const cases = [{}, []];
+    for(let param of cases){
+      const emp = new Employee({firstName: param, lastName: param, department: param})
+      emp.validate(err => {
+        expect(err.errors.firstName && err.errors.lastName && err.errors.department).to.exist;
+      })
+    }
+  })
+
+  it('should throw NO error, if parameters are correct', () => {
+    const emp = new Employee({firstName: 'name', lastName: 'surname', department: 'IT'})
+      emp.validate(err => {
+        expect(err).to.not.exist;
+    })
+  })
+  
+  after(() => {
+    mongoose.models = {};
+  });
+});
